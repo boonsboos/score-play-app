838c1d7d416a4eb8bfb665186e925aabfa491ece
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2025-11-23 20:33:37 +0100

    Fixed issue with Material Icons

--- app/src/main/kotlin/nl/connectplay/scoreplay/screens/RegisterScreen.kt
+++ app/src/main/kotlin/nl/connectplay/scoreplay/screens/RegisterScreen.kt
@@ -3,6 +3,7 @@ package nl.connectplay.scoreplay.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,13 +11,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -95,17 +97,16 @@ fun RegisterScreen(
                 keyboardType = KeyboardType.Password,
                 imeAction = ImeAction.Next
             ),
-//            trailingIcon = {
-//                IconButton (onClick = { showPassword = !showPassword }) {
-//                    Icon(
-//                        painter = painterResource(
-//                            if (showPassword) R.drawable.ic_visibility_off
-//                            else R.drawable.ic_visibility
-//                        ),
-//                        contentDescription = null
-//                    )
-//                }
-//            },
+            trailingIcon = {
+                IconButton (onClick = { showPassword = !showPassword }) {
+                    Icon(
+                        imageVector =
+                            if (showPassword) Icons.Default.Visibility
+                            else Icons.Default.VisibilityOff,
+                        contentDescription = "Visibility"
+                    )
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 24.dp)
@@ -150,8 +151,6 @@ fun RegisterScreen(
         ) {
             Text("Create account")
         }
-
-        Spacer(modifier = Modifier.height(12.dp))
 
         TextButton(onClick = onNavigateToLogin) {
             Text("Already have an account? Log in")
