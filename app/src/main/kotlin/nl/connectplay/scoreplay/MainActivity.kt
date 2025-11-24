bb140f78616f5b120886be8112e672909240a10c
boonsboos <boons@boonsboos.nl>
2025-11-24 21:08:02 +0100

    [FSA-223] top bar

--- app/src/main/kotlin/nl/connectplay/scoreplay/MainActivity.kt
+++ app/src/main/kotlin/nl/connectplay/scoreplay/MainActivity.kt
@@ -1,14 +1,19 @@
 package nl.connectplay.scoreplay
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import nl.connectplay.scoreplay.ui.components.Navigator
+import nl.connectplay.scoreplay.ui.components.ScorePlayTopBar
 import nl.connectplay.scoreplay.ui.theme.ScorePlayTheme
 
 class MainActivity : ComponentActivity() {
