3763c9d0c8253764fd26bb3d449662cb92c76b32
mastermooss <60898036+mastermooss@users.noreply.github.com>
2025-11-27 10:57:43 +0100

    [FSA-222] Feat/add bottom bar (#4)
    I need to add some comments

--- app/src/main/kotlin/nl/connectplay/scoreplay/MainActivity.kt
+++ app/src/main/kotlin/nl/connectplay/scoreplay/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import nl.connectplay.scoreplay.ui.components.Navigator
 import nl.connectplay.scoreplay.ui.theme.ScorePlayTheme
@@ -17,21 +15,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScorePlayTheme {
-                /* A Scaffold is a layout structure that provides slots
-                 * for the most common top-level material components:
-                 * TopBar, BottomBar and FloatingActionButton.
-                 * 
-                 * But add those top-level components to the screen itself,
-                 * not here because not all screens need such component
-                 * think about sign-in that does not need an bottombar
-                 */
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Navigator(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    )
-                }
+                Navigator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
             }
         }
     }
