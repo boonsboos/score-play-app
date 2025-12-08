660c222246a4aefa672f22a623d15d691b8bf9e2
mastermooss <60898036+mastermooss@users.noreply.github.com>
2025-12-08 20:11:23 +0100

    Update LoginUiState.kt

--- app/src/main/kotlin/nl/connectplay/scoreplay/viewModels/login/LoginUiState.kt
+++ app/src/main/kotlin/nl/connectplay/scoreplay/viewModels/login/LoginUiState.kt
@@ -1,14 +1,22 @@
 package nl.connectplay.scoreplay.viewModels.login
 
 /**
- * Events that the login screen can react to.
+ * Represents all ui data for the login screen
  *
- * These are one-time actions. They do not stay in the state.
- * The UI listens for these events and does something once.
+ * When something is changing, the screen wil changes to
  *
- * For example:
- * - Success → go to the next screen.
+ * @property credentials this can be username or email
+ * @property password password what the user needs
+ * @property showPassword for the toggle of hide or unhide password
+ * @property isLoading necessary for the check if we are logging in or not
+ * @property errorMessage for the errors
+ * @property isFormValid the check for the valid value in the inputfields
  */
-sealed class LoginEvent {
-    object Success : LoginEvent()
-}
+data class LoginUiState(
+    val credentials: String = "",
+    val password: String = "",
+    val showPassword: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val isFormValid: Boolean = false
+)
