e7eb8aaf421a0c057a20683a185420941d171e46
Mingull <nielsplug@outlook.com>
2026-01-13 20:56:08 +0100

    Fixed navigator wrong start screen

--- app/src/main/kotlin/nl/connectplay/scoreplay/ui/components/Navigator.kt
+++ app/src/main/kotlin/nl/connectplay/scoreplay/ui/components/Navigator.kt
@@ -51,7 +51,7 @@ fun Navigator(modifier: Modifier = Modifier) {
         }
     }
 
-    val start = if (tokenState.token != null) Screens.Home else Screens.Register
+    val start = if (tokenState.token != null) Screens.Home else Screens.Login
 
     val backStack = rememberNavBackStack(start)
 
